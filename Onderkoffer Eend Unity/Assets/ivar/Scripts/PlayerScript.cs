5528a4aacadcd8b3470419a27c696d5185fa236e
Watenk <Ivar.nuij@gmail.com>
2022-06-03 17:28:26 +0200

    Added more multiplayer

--- Onderkoffer Eend Unity/Assets/ivar/Scripts/PlayerScript.cs
+++ Onderkoffer Eend Unity/Assets/ivar/Scripts/PlayerScript.cs
@@ -1,34 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerScript : MonoBehaviour
 {
     public float speed;
     Rigidbody rb;
 
+    PhotonView view;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        view = GetComponent<PhotonView>();
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey("w"))
+        if (view.IsMine)
         {
-            rb.AddForce(transform.forward * speed);
-        }
-        if (Input.GetKey("d"))
-        {
-            rb.AddForce(transform.right * speed);
-        }
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce((transform.forward * -1) * speed);
-        }
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce((transform.right * -1) * speed);
+            if (Input.GetKey("w"))
+            {
+                rb.AddForce(transform.forward * speed);
+            }
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(transform.right * speed);
+            }
+            if (Input.GetKey("s"))
+            {
+                rb.AddForce((transform.forward * -1) * speed);
+            }
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce((transform.right * -1) * speed);
+            }
         }
     }
 }
